ab8bef5219eaa23fae650cd76f7d3ffd10a7d155
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2012-01-03 09:07:07 +0100

    Hello,

--- src/clojure_web_hello_world_ring/core.clj
+++ src/clojure_web_hello_world_ring/core.clj
@@ -1,7 +1,12 @@
 (ns clojure-web-hello-world-ring.core
-    (:use ring.adapter.jetty))
+    (:use ring.adapter.jetty)
+    (:use clojure.string))
+(defn extract-name [uri]
+    (replace-first uri "/" ""))
 (defn app [req]
     {:status 200
      :headers {"Content-Type" "text/html"}
-     :body    "Hello World!"})
+     :body    (if (= (:uri req) "/") 
+                  "Hello, World!"
+                  (str "Hello, " (extract-name (:uri req)) "!"))})
 (defn -main [] (run-jetty #'app {:port 8080}))
